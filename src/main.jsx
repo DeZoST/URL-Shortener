f4e00d99646e341e36264133c747e5e0d02b971f
DeZoST <zosted62400@gmail.com>
2024-08-26 21:46:31 +0200

    Change css files import order

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/utils.css";
+import "./styles/modern-normalize.css";
 import "./styles/style.css";
+import "./styles/utils.css";
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
